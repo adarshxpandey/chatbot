4e9f62a2db5b138463f06c0af79c98f207e53fcf
Adarsh <pandeyadarsh418@gmail.com>
2024-12-31 21:02:21 +0530

    Bind to 0.0.0.0 and use environment PORT

--- app.py
+++ app.py
@@ -39,4 +39,6 @@ def ask():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port,debug=True)
